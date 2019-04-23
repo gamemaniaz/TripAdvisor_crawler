983089f6698511e9f78771f155a87f7d4bd133f6
Louis <gamemaniaz@hotmail.com>
2019-04-23 21:18:03 +0800

    revamped script for singapore hotels

--- url_parser.py
+++ url_parser.py
@@ -1,63 +1,65 @@
-# -*- coding: utf-8 -*-
-import pandas as pd
-import numpy as np
+import csv, re
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import requests
-import time
-import csv
-import re
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-options = webdriver.ChromeOptions()
-options.add_argument('headless')
-target_url = 'https://www.tripadvisor.com.tw/Hotels-g294225-Indonesia-Hotels.html'
-driver = webdriver.Chrome('./chromedriver', chrome_options=options)
-driver.get(target_url)
-driver.maximize_window()
 
-soup = BeautifulSoup(driver.page_source, 'html.parser')
-domain = 'https://www.tripadvisor.com.tw'
+def main():
 
-# scrape page
-next_page = '//*[@id="taplc_main_pagination_bar_dusty_hotels_resp_0"]/div/div/div/span[2]'
-check_last_page = '#taplc_main_pagination_bar_dusty_hotels_resp_0 > div > div > div > div > span.pageNum.last.taLnk'
-page_down = "window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;"
-page_list = range(int(soup.select(check_last_page)[0].get('data-page-number')))
-print("Total number of page: {}".format(len(page_list)))
+    driver = webdriver.Chrome()
+    driver.get('https://www.tripadvisor.com.sg/Hotels-g294262-Singapore-Hotels.html')
 
-with open('./data/url_parser.csv', 'a') as csvfile:
-    fieldnames = ['hotel_id', 'hotel_name', 'n_comment', 'rank_in_country', 'url']
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
-    index = 0
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    page_numbers = soup.find_all('span', {'class':['pageNum', 'last']})
+    page_count = int(list(filter(lambda link: set(link.get('class')) == set(['pageNum', 'last', 'taLnk', '']), page_numbers))[0].text)
 
-    for p in page_list:
-        print('the number of page = {0}/{1}'.format(p+1, len(page_list)))
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        hotel_blocks = soup.find_all('div', {"class": "prw_rup prw_meta_hsx_responsive_listing ui_section listItem"})
+    hotels = []
+    hotel_id = 0
 
-        for element in hotel_blocks:
-            index += 1
-            hotel_name = element.find('div', {"class": "listing_title"}).text
-            url = domain+element.find('div', {"class": "listing_title"}).find('a').get('href')
-            n_comment = element.find('a', {"class": "review_count"}).text
+    for page in range(1, page_count + 1):
+
+        print('Scraping Page', page)
+
+        # navigate to next page
+        if page > 1:            
+            while True:
+                try:
+                    page_button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="taplc_main_pagination_bar_hotels_pagination_links_optimization_0"]/div/div/div/span[2]')))
+                    page_button.click()
+                    break
+                except:
+                    driver.implicitly_wait(1)
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+        # extract data from hotel blocks
+        hotel_tags = soup.find_all('div', {'class': 'prw_rup prw_meta_hsx_responsive_listing ui_section listItem'})
+        for hotel_tag in hotel_tags:
+            hotel_id += 1
+            hotel_name = hotel_tag.find('div', {'class': 'listing_title'}).text
+            url = 'https://www.tripadvisor.com.sg' + hotel_tag.find('div', {"class": 'listing_title'}).find('a').get('href')
+            n_comment = hotel_tag.find('a', {"class": "review_count"}).text
             n_comment = re.sub('[^0-9,]', "", n_comment).replace(',','')
-            rank_in_country = element.find('div', {"class": "popindex"}).text
-            writer.writerow(
-                            {
-                                'hotel_id':index,
-                                'hotel_name':hotel_name.encode("utf-8"),
-                                'n_comment':n_comment,
-                                'rank_in_country':rank_in_country.encode("utf-8"),
-                                'url':url
-                            }
-                           )
-        try:
-            driver.execute_script(page_down)
-            time.sleep(5)
-            driver.find_element_by_xpath(next_page).click()
-            time.sleep(8)
-        except:
-            print('in the end')
+            rank_in_country = hotel_tag.find('div', {"class": "popindex"}).text
+            hotel = {
+                'id': hotel_id,
+                'hotel_name': hotel_name.encode('utf-8'),
+                'n_comment': n_comment,
+                'rank_in_country': rank_in_country.encode('utf-8'),
+                'url': url
+            }
+            hotels.append(hotel)
 
-driver.quit()
+    # write extracted hotel data to csv
+    if hotels:
+        with open('data/hotels.csv', 'w', newline='') as csvfile:
+            fieldnames = ['id', 'hotel_name', 'n_comment', 'rank_in_country', 'url']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for hotel in hotels:
+                writer.writerow(hotel)
+
+
+if __name__ == '__main__':
+    main()
